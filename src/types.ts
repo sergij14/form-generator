62cf99a05656300a1858f4b96a571212f9f45779
sergij14 <sergi.jajanidze@gmail.com>
2022-11-26 18:29:47 +0400

    create form props

---
+++ src/types.ts
@@ -0,0 +1,23 @@
+import { SubmitHandler, FieldValues } from 'react-hook-form';
+
+type FieldSchema = {
+  type: 'text' | 'number' | 'array' | 'object';
+};
+
+type defaultProps = {
+  label: string;
+  placeholder?: string;
+};
+
+export type TextFieldProps = FieldSchema &
+  defaultProps & {
+    type: 'text';
+  };
+
+export type Field = TextFieldProps;
+
+export type Fields = Record<string, Field>;
+export interface FormProps {
+  fields: Fields;
+  onSubmit: SubmitHandler<FieldValues>;
+}
--- stories/Form.stories.tsx
+++ stories/Form.stories.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Meta, Story } from '@storybook/react';
-import {  } from '../src';
+import { Form, FormProps } from '../src';
 
 const meta: Meta = {
   title: 'Welcome',
-  component: Thing,
+  component: Form,
   argTypes: {
     children: {
       control: {
@@ -19,10 +19,20 @@ const meta: Meta = {
 
 export default meta;
 
-const Template: Story<Props> = args => <Thing {...args} />;
+const Template: Story<FormProps> = args => <Form {...args} />;
 
-// By passing using the Args format for exported stories, you can control the props for a component for reuse in a test
-// https://storybook.js.org/docs/react/workflows/unit-testing
+const fields: FormProps['fields'] = {
+  email: {
+    type: 'text',
+    label: 'email',
+  },
+};
+
 export const Default = Template.bind({});
 
-Default.args = {};
+Default.args = {
+  fields,
+  onSubmit: values => {
+    console.log(values);
+  },
+};
